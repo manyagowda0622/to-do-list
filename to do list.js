1f364ef12a8fea593a97012840f592bd00a831f8
manyagowda0622 <manyagowda0609@gmail.com>
2026-01-30 12:14:51 +0530

    Add files via upload
    All changes commit

---
+++ to do list.js
@@ -0,0 +1,22 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${taskText}
+        <button class="delete-btn" onclick="deleteTask(this)">X</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
